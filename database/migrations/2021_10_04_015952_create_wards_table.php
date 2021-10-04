66b218110fd3dc13d3a70ab6c39f28102087049a
JosueOb <josuericardocando@hotmail.com>
2021-10-03 21:11:51 -0500

    Create wards migration

---
+++ database/migrations/2021_10_04_015952_create_wards_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wards', function (Blueprint $table) {
+            $table->id();
+
+            /*required columns*/
+            $table->string('name', 45);
+            $table->string('location', 45);
+            $table->string('description');
+            $table->boolean('state')->default(true);
+
+            /*special columns*/
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wards');
+    }
+}
