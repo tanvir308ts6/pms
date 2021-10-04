0763a05405572bac0979bebbd382bd4261f6d91b
JosueOb <josuericardocando@hotmail.com>
2021-10-03 21:56:05 -0500

    Create repots migration

---
+++ database/migrations/2021_10_04_023744_create_reports_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+
+            /*required columns*/
+            $table->string('title');
+            $table->string('description');
+            $table->boolean('state')->default(true);
+
+            /*special columns*/
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
